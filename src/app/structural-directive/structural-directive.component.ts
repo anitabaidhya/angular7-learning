60c96aaa932c279758fc3ab539fb00b8bb41358b
anitabaidhya <bernhardtiananita@gmail.com>
2019-01-11 11:37:38 +1100

    switch case

--- src/app/structural-directive/structural-directive.component.ts
+++ src/app/structural-directive/structural-directive.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./structural-directive.component.css']
 })
 export class StructuralDirectiveComponent implements OnInit {
-  public displayName = true;;
+  public displayName = true;
+  public color = "red";
   constructor() { }
 
   ngOnInit() {
