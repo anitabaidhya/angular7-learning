0a3100a7d9ee17acd500f07ebc9c3bb932792356
anitabaidhya <bernhardtiananita@gmail.com>
2019-01-14 19:37:11 +1100

    using ngif and ngswitch

--- src/app/structural-directive/structural-directive.component.ts
+++ src/app/structural-directive/structural-directive.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class StructuralDirectiveComponent implements OnInit {
   public displayName = true;
   public color = "red";
+  public date = new Date();
   constructor() { }
 
   ngOnInit() {
